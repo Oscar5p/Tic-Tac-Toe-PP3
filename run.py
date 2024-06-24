dc56b450ba75fa1790a1b4c45921859be75edfc1
Oscar5p Kron <lakro001@live.se>
2024-06-24 10:06:25 +0000

    adding while playing and if command

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 from helpers import draw_board
-
+import os
 spots = {1: "1", 2: "2", 3: "3", 4: "4", 5: "5",
          6: "6", 7: "7", 8: "8", 9: "9"}
 
-draw_board()
+playing = True
+
+while playing:
+    # Reset the screen
+    os.
